d5e49b4d01c8c451f139c9738764339637a97dc0
tharii99 <95270183+tharii99@users.noreply.github.com>
2022-04-22 15:34:53 +0530

    New Changes

---
+++ Frontend/Project/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { VTemFComponent } from './vtem-f/vtem-f.component';
+import { SignupComponent } from './signup/signup.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    VTemFComponent,
+    SignupComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Frontend/Project/src/app/vtem-f/vtem-f.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vtem-f',
+  templateUrl: './vtem-f.component.html',
+  styleUrls: ['./vtem-f.component.css']
+})
+export class VTemFComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
