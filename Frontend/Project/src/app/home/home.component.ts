a431094d0faeebd45cc4d6018d0fc086e055b3a1
tharii99 <95270183+tharii99@users.noreply.github.com>
2022-04-23 12:31:32 +0530

    Update home.component.ts

--- Frontend/Project/src/app/home/home.component.ts
+++ Frontend/Project/src/app/home/home.component.ts
@@ -1,11 +1,9 @@
 import { Component} from '@angular/core';
 
 @Component({
-  
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
-
+  selector: 'home',
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.css']
 });
 
 
