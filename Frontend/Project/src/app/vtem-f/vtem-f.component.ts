ca3a0ace3ac9eb3e7d1f4b052a89cfdaa02d00ab
tharii99 <95270183+tharii99@users.noreply.github.com>
2022-04-23 12:35:59 +0530

    Update vtem-f.component.ts

--- Frontend/Project/src/app/vtem-f/vtem-f.component.ts
+++ Frontend/Project/src/app/vtem-f/vtem-f.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-vtem-f',
-  templateUrl: 'vtem-f.component.html',
+  templateUrl: 'index.html',
   styleUrls: ['vtem-f.component.css']
 })
 export class VTemFComponent implements OnInit {
