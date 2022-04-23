100a9eadefc48219091e63cfda4db54a62dfa2d0
tharii99 <95270183+tharii99@users.noreply.github.com>
2022-04-23 12:11:17 +0530

    Update home.component.ts

--- Frontend/Project/src/app/home/home.component.ts
+++ Frontend/Project/src/app/home/home.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core'
 
-// @Component({
+@Component({
   
-//   selector: 'app-home',
-//   templateUrl: './home.component.html',
-//   styleUrls: ['./home.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 
-// });
+});
 
-export class HomeComponent implements OnInit {
+@Injectable()
+export abstract class HomeComponent implements OnInit {
 
   constructor() { }
 
